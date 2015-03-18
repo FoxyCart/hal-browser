de4f7f8e3ccfdaef1027ee1434d787991899ff0a
Luke Stokes <luke.stokes@gmail.com>
2015-03-17 22:29:10 -0500

    Fixing XSS concerns at client.js level.

--- js/hal/http/client.js
+++ js/hal/http/client.js
@@ -3,8 +3,19 @@ HAL.Http.Client = function(opts) {
   this.defaultHeaders = { 'Accept': 'application/hal+json, application/json, */*; q=0.01' };
 };
 
+HAL.Http.Client.prototype.filterHostName = function(url) {
+  var link = document.createElement("a");
+  link.href = url;
+  if (link.hostname != window.location.hostname) {
+    console.log('invalid url domain for ' + window.location.hostname + ': ' + link.hostname);
+    url = '/';
+  }
+  return url;
+};
+
 HAL.Http.Client.prototype.get = function(url) {
   var self = this;
+  url = this.filterHostName(url);
   this.vent.trigger('location-change', { url: url });
   var jqxhr = $.ajax({
     url: url,
@@ -25,6 +36,7 @@ HAL.Http.Client.prototype.get = function(url) {
 HAL.Http.Client.prototype.request = function(opts) {
   var self = this;
   opts.dataType = 'json';
+  opts.url = this.filterHostName(opts.url);
   self.vent.trigger('location-change', { url: opts.url });
   return jqxhr = $.ajax(opts);
 };
