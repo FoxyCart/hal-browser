8d8c3c2b4b2f6c4de482b924208ddeaefcde83e5
Luke Stokes <luke.stokes@gmail.com>
2016-10-06 17:27:03 -0500

    Adding a flag to our documentation window for layout purposes.

--- js/hal/views/documentation.js
+++ js/hal/views/documentation.js
@@ -2,6 +2,6 @@ HAL.Views.Documenation = Backbone.View.extend({
   className: 'documentation',
 
   render: function(url) {
-    this.$el.height(500).html('<iframe src=' + url + '></iframe>');
+    this.$el.height(500).html('<iframe src="' + url + '?layout=false"></iframe>');
   }
 });
